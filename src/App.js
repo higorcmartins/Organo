0aeb157dfb029f59d0ac5002462f54a4fcf01883
Higor <higorcmartins@gmail.com>
2024-03-13 18:37:53 -0300

    Adicionando o Formulario

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Banner from './componentes/Banner/Banner';
+import Formulario from './componentes/Formulario/Formulario';
 
 function App() {
   return (
     <div className="App">
       <Banner/>
+      <Formulario/>
     </div>
   );
 }
