c924bde85560b4d8e2ee6f76487ac7cd7327322c
Higor <higorcmartins@gmail.com>
2024-03-14 19:49:28 -0300

    Adicionando eventos do botão do formulario

--- src/componentes/Formulario/Formulario.js
+++ src/componentes/Formulario/Formulario.js
@@ -8,19 +8,24 @@ const Formulario = () =>{
     const times = [
         'Progamação',
         'Front-End',
-        'Data Scince',
+        'Data Science',
         'DevOps',
         'UX e Design',
         'Mobile',
         'Inovação e Gestão'
     ]
 
+    const aoSalvar = (evento) => {
+        evento.preventDefault()
+        console.log('Form foi submetido')
+    }
+
     return(
         <section className='formulario'>
-            <form>
+            <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card do colaborador.</h2>
-                <CampoTexto label="Nome" placeholder="Digite seu nome"/>
-                <CampoTexto label="Cargo" placeholder="Digite seu cargo"/>
+                <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu nome"/>
+                <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite seu cargo"/>
                 <CampoTexto label="Imagem" placeholder="Informe o endereço da imagem"/>
                 <ListaSuspensa label= "Time" itens={times}/>
                 <Botao>
